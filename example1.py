e81de0d9a80f6e54eca6b09e04fa26fd5cc94d90
Cygnios <57601431+Cygnios@users.noreply.github.com>
2020-01-16 14:02:32 -0600

    Update example1.py

--- example1.py
+++ example1.py
@@ -6,20 +6,19 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'E1'
-strategy_name = 'Betray'
-strategy_description = 'Always betray.'
+team_name = 'GloryfulTomatoes'
+strategy_name = 'Complex'
+strategy_description = 'Tries to read the opposing team.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    #This example player always betrays.      
-    return 'b'
+    my_history = 'c'
+    if my_history[-1] and their_history[-0] == 'b':
+        return 'b'
+    else:
+        return 'c'
+    if my_history[-1] and their_history[-1] == 'c':
+        return 'b'
+    if their_history[-1] and their_history[-2] == 'c':
+        return 'b'
+    if their_history[-1] == 'b':
+        return 'c'
